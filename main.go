57a308b3c35ddd5d0c426723c53d873cc036a161
shawn <vindamocx@gmail.com>
2023-02-21 21:33:37 +0800

    add gin

--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"embed"
+	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
+	"io/fs"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 )
 
+var FS embed.FS
+
 func main() {
-	ui, err := lorca.New("https://www.baidu.com", "", 800, 600)
+	go func() {
+		gin.SetMode(gin.DebugMode)
+		router := gin.Default()
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))
+		router.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			}
+		})
+		router.Run(":8080")
+	}()
+	ui, err := lorca.New("http://localhost:8080", "", 800, 600)
 	if err != nil {
 		return
 	}
